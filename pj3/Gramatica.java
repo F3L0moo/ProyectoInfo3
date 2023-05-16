f1bcf2f870ddbbbc353dba8aebf66f1038d07b4e
F3L0moo <109702397+F3L0moo@users.noreply.github.com>
2023-05-16 03:54:22 +0000

    Inicio de proyecto

---
+++ pj3/Gramatica.java
@@ -0,0 +1,26 @@
+import javax.swing.plaf.nimbus.State;
+
+public class Gramatica{
+
+	public static void main(String[] args) throws Exception{
+		//Su codigo aqui
+		if (args.length == 0) {
+			System.out.println("El uso correcto del sistema es: *Nombre de archivo* (-afn, -afd o -check) *Nombre de archivo de salida");
+			return;
+		}
+		if(args[1].equals("-afn")) {
+
+		}
+		if(args[1].equals("-afd")){
+
+		}
+		if(args[1].equals("-check")){
+
+		}
+
+		//################   AFN      ########################################
+		static void GLDaAFN(String nombreArchivo) throws Exception{
+			return;
+		}
+	}
+}
